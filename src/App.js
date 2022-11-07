b29976e6ba50be8650d09e5d47b433bed9f383de
Kotireddy Kamireddy <kotireddyk0485@gmail.com>
2022-11-07 21:49:35 +0530

    feat: header component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//      <h1>Interduction</h1>
+//      <p>Thhis is a para</p>
+//     </div>
+//   );
+// }
+class App extends React.Component{
+  render(){
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      Hello World
-    </div>
-  );
+        <div className="App">
+          <Header />
+        </div>
+      );
+  }
 }
 
 export default App;
+
+// // two type component 
+// 1.class component
+// 2. function component
+//Header component
+//Content component
+//footer 
---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+class Header extends React.Component{
+    render(){
+        return(
+            <div className="headerContent">
+            <h1>Header Component</h1>
+            </div>
+        )
+    }
+};
+
+export default Header;
